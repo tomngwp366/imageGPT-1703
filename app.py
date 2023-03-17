5a4a758325cfb0318e48f85210940f5e1388e834
tomngwp366 <128126435+tomngwp366@users.noreply.github.com>
2023-03-17 15:43:15 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install replicate
+
+
+# In[2]:
+
+
+from flask import Flask,request,render_template
+import replicate
+import os
+
+os.environ["REPLICATE_API_TOKEN"] = "35d9aafd75e3ad9633c78a913f62371345c27eb3"
+model = replicate.models.get("tstramer/midjourney-diffusion")
+version = model.versions.get("436b051ebd8f68d23e83d22de5e198e0995357afef113768c20f0b6fcef23c8b")
+
+app = Flask(__name__)
+
+@app.route("/",methods=["GET","POST"])
+def index():
+    if request.method == "POST":
+        p = request.form.get("txt")
+        inputs = {"prompt":p}
+        out = version.predict(**inputs)
+        return(render_template("index.html",result=out[0]))
+    else:
+        return(render_template("index.html",result="waiting"))
+
+if __name__ == "__main__":
+    app.run()
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
